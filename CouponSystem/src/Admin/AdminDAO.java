f7cf69b27039f108c2061ef41e8f74b9de001d8f
Yocheved  Tochner <44393518+yochevedt@users.noreply.github.com>
2019-06-04 16:18:14 +0300

    update - AdminDAO and AdminDBDO

--- CouponSystem/src/Admin/AdminDAO.java
+++ CouponSystem/src/Admin/AdminDAO.java
@@ -1,7 +1,18 @@
 package Admin;
+import java.util.Set;
+
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
+import Company.*;
 
 public interface AdminDAO {
 
-	void AdminLogin();
+	void AdminLogin() throws Throwable, IllegalAccessException;
+	
+	
+	
+	
+		 
 
+	 
 }
